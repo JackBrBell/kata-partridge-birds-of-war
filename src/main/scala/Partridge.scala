f0861c7ed52b78ad46de8ba8435422e91e814820
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-02-08 15:59:02 +0000

    Part (possibly) working

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -1,4 +1,4 @@
-object Partridge {
+object Partridge extends App{
 
   // IMPORTANT: Do not change the signatures of the functions declared below.
   // See the README file for explanations of what each function should do.
@@ -11,15 +11,17 @@ object Partridge {
     val accepted = Array ("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
 
     //filter args using accepted
-    val filtered = args.filter(_.contains(accepted))
+    //val statements = accepted.mkString(" ")
+    val filtered = args.count(accepted contains _)
     //output = filtered
+    println(filtered)
 
     //check is filtered is empty
-    if (filtered.isEmpty) {
+    if (filtered == 0) {
       "Lynn, I've pierced my foot on a spike!!"
     } else {
-      val exPoints = "!"*filtered.length
-      s"Mines a pint!$exPoints"
+      //val exPoints = "!"*filtered.length
+      s"Mine's a Pint" + "!"*filtered
     }
     //If empty, return FOOT SPIKE!!
     //If not, check length of filtered
@@ -28,11 +30,13 @@ object Partridge {
   }
 
   def apple(x: Any): String = {
-    ???
+  ???
   }
 
   def london(args: Array[String]): String = {
     ???
-  }
+}
 
+
+  part(Array("sausage", "pelvis"))
 }
