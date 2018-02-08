0b2defa0723925d506c99c6ff703a097776728e4
jpatton638 <jpatton638@gmail.com>
2018-02-08 13:31:48 +0000

    Almost finished apple

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -8,11 +8,30 @@ object Partridge {
   }
 
   def apple(x: Any): String = {
-    ???
+
+    x match {
+      case _: Int => process(_)
+      case s: String =>
+        try {
+          process(s.toInt)
+        } catch {
+          case ex: NumberFormatException => "Not a number"
+        }
+      case _ => "Not a number"
+    }
+
+    def process(num: Int): String = {
+      val squared = num*num
+      if (squared >= 1000) {
+        "It's hotter than the sun!!"
+      } else {
+        "Help yourself to a honeycomb Yorkie for the glovebox."
+      }
+    }
+
   }
 
   def london(args: Array[String]): String = {
     ???
   }
-
 }
