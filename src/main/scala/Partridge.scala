6328ecc9c7f6a0b1761c13b7ab11d4dca3e87a12
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-02-08 14:34:30 +0000

    part almost done

--- src/main/scala/Partridge.scala
+++ src/main/scala/Partridge.scala
@@ -4,7 +4,27 @@ object Partridge {
   // See the README file for explanations of what each function should do.
 
   def part(args: Array[String]): String = {
-    ???
+
+    //Input array = args
+
+    //Array of accepted strings = accepted
+    val accepted = Array ("Partridge", "PearTree", "Chat", "Dan", "Toblerone", "Lynn", "AlphaPapa", "Nomad")
+
+    //filter args using accepted
+    val filtered = args.filter(_.contains(accepted))
+    //output = filtered
+
+    //check is filtered is empty
+    if (filtered.isEmpty) {
+      "Lynn, I've pierced my foot on a spike!!"
+    } else {
+      val exPoints = "!"*filtered.length
+      s"Mines a pint!$exPoints"
+    }
+    //If empty, return FOOT SPIKE!!
+    //If not, check length of filtered
+    //length of filtered = number of !
+    // MINES A PINT!
   }
 
   def apple(x: Any): String = {
